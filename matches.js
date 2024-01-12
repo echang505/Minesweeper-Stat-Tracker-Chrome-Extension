36fbb0afab5ac25f31a5a5206b2d3734c209758e
Eric Chang <eric.chang1000@gmail.com>
2024-01-12 16:02:20 -0500

    initial tests with content scripts

---
+++ matches.js
@@ -0,0 +1,14 @@
+const button = document.createElement("input");
+button.type = "button";
+button.value = "Change background to red";
+
+const child = document.body.firstChild;
+document.body.insertBefore(button, child);
+
+button.addEventListener("click", () => {
+    console.log("button click");
+    document.body.style.backgroundColor = "red";
+});
+
+console.log(button)
+console.dir(button);
